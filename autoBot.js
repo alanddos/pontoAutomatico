16196441caba96283bc27f1e301bae46d15086bb
AlanDdos <alanddos>
2021-11-11 13:01:40 -0400

    Fechando confirmação apos bater o ponto

--- autoBot.js
+++ autoBot.js
@@ -58,32 +58,34 @@ const ChecarTime = async (diasSemana, horarios, page) => {
         let hora = format(new Date(), "HH")
         if (horarios.indexOf(String(hora)) !== -1) {
           let minutos = format(new Date(), "mm").toString()
-          console.log(`${hora}:${minutos}`)
           if (minutos === "00") {
-            console.log(`Batendo ${horarios.indexOf(String(hora)) + 1 }º ponto: ${hora}:${minutos}`)
-             BaterPonto(page);
+            console.log(`Batendo ${horarios.indexOf(String(hora)) + 1}º ponto: ${hora}:${minutos}`)
+            BaterPonto(page);
           }
         }
-      } 
+      }
     } catch (error) {
       console.log(error)
-    }    
+    }
   }, 60000);
 };
 
 const BaterPonto = async (page) => {
   let url = `https://app.pontomaisweb.com.br/#/meu_ponto/registro_de_ponto`;
   let currentUrl = page.url();
-  if (currentUrl !== url){
+  if (currentUrl !== url) {
     await page.goto(url);
     await sleep(30000);
   }
-  
+
   const pontoButton = (await page.$x("//button[.//text() = 'Registrar ponto']"))[0];
   await pontoButton.click();
-  // await sleep(8000); // Aguardar para confirmar a mensagem de registro
-  // const okButton = (await page.$x("//button[.//text() = 'OK']"))[0];
-  // await okButton.click();
+  await sleep(8000); // Aguardar para fechar a mensagem de registro
+  await page.evaluate(() => {
+    let elements = document.getElementsByClassName('close ng-scope');
+    for (let element of elements)
+      element.click();
+  });
   return page;
 
 };
